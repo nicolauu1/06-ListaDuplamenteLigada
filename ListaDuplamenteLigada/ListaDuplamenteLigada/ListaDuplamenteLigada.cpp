411a29b481344531448b9c9d26eb865807d85bd5
nicolau <61714680+nicolauu1@users.noreply.github.com>
2023-09-20 23:44:12 -0300

    Update ListaDuplamenteLigada.cpp

--- ListaDuplamenteLigada/ListaDuplamenteLigada/ListaDuplamenteLigada.cpp
+++ ListaDuplamenteLigada/ListaDuplamenteLigada/ListaDuplamenteLigada.cpp
@@ -150,17 +150,50 @@ void inserirElemento()
 // funções a serem implementadas no exericio
 void exibirReverso()
 {
-
+	if (primeiro == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
+	else {
+		cout << "Elementos: \n";
+		NO* aux = ultimo;
+		while (aux != NULL) {
+			cout << aux->valor << endl;
+			aux = aux->ant;
+		}
+	}
 }
 
 void excluirPrimeiroElemento()
 {
+	if (primeiro == NULL)
+	{
+		cout << "Lista vazia \n";
+	}
+	else {
+		NO* aux = primeiro;
+		primeiro = aux->prox;
+		primeiro->ant = NULL;
+		free(aux);
 
+		cout << "O elemento foi excluido \n";
+	}
 }
 
 void excluirUltimoElemento()
 {
+	if (ultimo == NULL)
+	{
+		cout << "Lista vazia \n";
+	}
+	else {
+		NO* aux = ultimo;
+		ultimo = aux->ant;
+		ultimo->prox = NULL;
+		free(aux);
 
+		cout << "O elemento foi excluido \n";
+	}
 }
 
 
